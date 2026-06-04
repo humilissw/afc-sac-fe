77d11267db05c6412cd24b2c6c86e6e8c135b071
cr1pto <20729543+cr1pto@users.noreply.github.com>
2026-06-03 20:34:23 -0700

    init commit - moved from apostolic-faith-sacramento

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  // output: 'export',
+  output: 'standalone',
+  images: { unoptimized: true },
+//  allowImportingTsExtensions: true,
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+module.exports = nextConfig
